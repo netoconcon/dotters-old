10174110c5284faae9b0ac3d4691c8075a6a5862
Neto Concon <concon.neto2@gmail.com>
2018-12-08 22:55:25 -0200

    add team controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'teams/index'
+  get 'teams/create'
+  get 'teams/new'
+  get 'teams/edit'
+  get 'teams/show'
+  get 'teams/update'
+  get 'teams/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
