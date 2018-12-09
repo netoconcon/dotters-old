878722240272ff3ab27893c22effd3d6f6f493f0
Neto Concon <concon.neto2@gmail.com>
2018-12-09 10:00:06 -0200

    add athlete controler

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,22 +1,44 @@
 class AthletesController < ApplicationController
   def index
+    @athletes = Athlete.all
   end
 
   def show
+    @athlete = Athlete.find(params[:id])
   end
 
   def new
+    @athlete = Athlete.new
   end
 
   def create
+    @athlete = Athlete.new(athlete_params)
+    if @athlete.save
+      redirect_to team_path(@team)
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @athlete = Athlete.find(params[:id])
   end
 
   def update
+    @athlete = Athlete.find(params[:id])
+    @athlete.update(athlete_params)
+    redirect_to teams_path(@team)
   end
 
   def destroy
+    @athlete = Athlete.find(params[:id])
+    @athlete.destroy
+    redirect_to team_path(@team)
+  end
+
+  private
+
+  def athlete_params
+    params.require(:athlete).permit(:name, :job1, :job2, :job3)
   end
 end
