c9b582ad33dc5ef33385e57b97885e39426443ad
Neto Concon <concon.neto2@gmail.com>
2018-12-08 23:54:42 -0200

    teams index

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,22 +1,40 @@
 class TeamsController < ApplicationController
   def index
-  end
-
-  def create
-  end
-
-  def new
-  end
-
-  def edit
+    @teams = Team.all
   end
 
   def show
+    @team = Team.find(params[:id])
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
   end
 
   def update
   end
 
   def destroy
+    @team = Team.find(params[:id])
+    @team.destroy
+    redirect_to teams_path
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name, :trekking, :mtb, :kayaking, :orienteering, :vertical)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
-  get 'teams/index'
-  get 'teams/create'
-  get 'teams/new'
-  get 'teams/edit'
-  get 'teams/show'
-  get 'teams/update'
-  get 'teams/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :teams
   get 'home', to: 'pages#home'
   get 'live', to: 'pages#live'
-  get 'teams', to: 'pages#teams'
   get 'rank', to: 'pages#rank'
 end
